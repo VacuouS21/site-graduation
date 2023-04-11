d1f2acd3a4139fd713d2bfffa5460f485302a530
VacuouS <vacuous21@gmail.com>
2023-04-11 18:58:34 +0300

    Первый каркас

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Navbar,Container,Nav,NavDropdown,Button,Form} from "react-bootstrap";
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header";
+
+function App() {
+  return (
+      <header><Header/></header>
+
+  );
+}
+
+export default App;
